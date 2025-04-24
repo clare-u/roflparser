bf4ebff3d90fd660cc516636edb33e7807bb018d
clare-u <clare.u.dev@gmail.com>
2025-04-25 01:08:55 +0900

    feat: 배포 주소 cors 해결

--- back/roflparser/src/main/java/com/example/roflparser/config/WebConfig.java
+++ back/roflparser/src/main/java/com/example/roflparser/config/WebConfig.java
@@ -12,6 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
                 .allowedOrigins("http://localhost:3000") // 클라이언트 도메인 허용
+                .allowedOrigins("http://roflbot.kro.kr")
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 쿠키 전송 허용
