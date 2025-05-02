bdbed3aefa73320172b18cc44f12d8319a57d30a
clare-u <clare.u.dev@gmail.com>
2025-05-02 12:22:58 +0900

    [front] feat: roflbot에 구글 애널리틱스 태그 추가

--- front/roflparser/src/app/layout.tsx
+++ front/roflparser/src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Toaster } from "sonner";
 import QueryProvider from "@/components/QueryProvider";
 import Navigation from "@/components/navigation/Navigation";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "롤내전봇",
@@ -17,12 +18,32 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-head" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KV8PQ9PC');
+          `}
+        </Script>
         <link
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined&display=swap"
           rel="stylesheet"
         />
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KV8PQ9PC"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <QueryProvider>
           <Navigation />
           <div className="flex h-full flex-col items-center justify-center">
